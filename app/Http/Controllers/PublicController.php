c0b69730fe12135afe45f1b6bd92b789e98a1305
Arianna Foi <arianna.foi@live.it>
2023-07-26 12:41:49 +0200

    inizio user2

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\cr;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function home() {
+
+        $articles = Article::take(6)->get()->orderBy('created_at');
+        dd($articles);
         return view('welcome');
     }
 }
