52e9b4e740bf28984714a6fa8e789f3e27880737
Ab0027 <alfonsoborromeo@hotmail.it>
2023-08-01 20:45:51 +0200

    Inserito alertSuccess in components

---
+++ resources/views/components/alertSuccess.blade.php
@@ -0,0 +1,19 @@
+<section class="modal container" id="alertSuccess">
+
+    <div class="modal__container" id="modal-container">
+        <div class="modal__content">
+            <div class="modal__close close-modal" title="Close">
+                <i class='bx bx-x'></i>
+            </div>
+
+                <img src="spunta.jpg" alt="" class="modal__img">
+
+            <h1 class="modal__title">Operazione effettuata con successo!</h1>
+
+            <button class="modal__button modal__button-width">
+                Clicca per tornare alla Home
+            </button>
+
+        </div>
+    </div>
+</section>
