c4c8a228da03fedbdf557c693d56b2fd9978cabe
Arianna Foi <arianna.foi@live.it>
2023-08-02 12:58:57 +0200

    inizio implementazione lingue

---
+++ lang/en/ui.php
@@ -0,0 +1,42 @@
+<?php 
+
+return [
+
+    // HOME
+    'latestArticles' => "Our latest articles",
+    'subtitleHeader' => "The fast response to your needs",
+    'access' => 'Login',
+    'register' => 'Register',
+    'categories' => 'Categories',
+    'announcements' => 'Articles',
+    'createAnnouncement' => 'Create your article',
+    'language' => 'Language',
+    'becomeRevisor' => 'Do you want to work with us?',
+    'becomeRevisorBtn' => 'Become a revisor',
+
+    // LOGIN-REGISTER FORM
+    'welcomeBack' => 'Welcome back!',
+    'signUpRequest' => 'Do you not have an account already? Sign up now!',
+    'welcome' => 'Welcome!',
+    'confirmationPassword' => 'Confirm Password',
+    'signInRequest' => 'Are you already registered? Sign in now!',
+
+    // ANNUNCI
+    'allAnnouncements' => 'All Articles',
+    'title' => 'Title',
+    'description' => 'Description',
+    'price' => 'Price',
+    'category' => 'Category',
+    'createAnnouncementBtn' => 'Create article',
+    'allAnnouncementOf' => 'All articles in',
+
+    'noAnnouncement' => 'There are no articles in this category yet!',
+    'newOne' => 'Be the first to publish:',
+
+    // CARD & DETAILS
+    'findOut' => 'Find out more',
+    'detailOf' => 'Details of',
+    'publish' => 'Published on',
+    'from' => 'by',
+    
+];
---
+++ lang/it/ui.php
@@ -0,0 +1,43 @@
+<?php 
+
+return [
+
+    // HOME
+    'latestArticles' => "Ecco gli ultimi annunci inseriti:",
+    'subtitleHeader' => "La risposta rapida ai tuoi bisogni",
+    'access' => 'Accedi',
+    'register' => 'Registrati',
+    'categories' => 'Categorie',
+    'announcements' => 'Annunci',
+    'createAnnouncement' => 'Crea il tuo annuncio',
+    'language' => 'Lingua',
+    'becomeRevisor' => 'Vuoi lavorare con noi?',
+    'becomeRevisorBtn' => 'Diventa revisore',
+
+    // LOGIN-REGISTER FORM
+    'welcomeBack' => 'Bentornato!',
+    'signUpRequest' => 'Non hai un account? Registrati!',
+    'welcome' => 'Benvenuto!',
+    'confirmationPassword' => 'Conferma Password',
+    'signInRequest' => 'Sei già registrato? Accedi!',
+
+    // ANNUNCI
+    'allAnnouncements' => 'Tutti Gli Annunci',
+    'title' => 'Titolo',
+    'description' => 'Descrizione',
+    'price' => 'Prezzo',
+    'category' => 'Categoria',
+    'createAnnouncementBtn' => 'Crea annuncio',
+    'allAnnouncementOf' => 'Tutti gli annunci di',
+
+    'noAnnouncement' => 'Non sono presenti annunci per questa categoria!',
+    'newOne' => 'Pubblicane uno:',
+
+    // CARD & DETAILS
+    'findOut' => 'Scopri di più',
+    'detailOf' => 'Dettaglio di',
+    'publish' => 'Pubblicato il',
+    'from' => 'da',
+    
+
+];
