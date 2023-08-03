af03961bb0d42ca74a824b90f4b6019aac85c101
Loremzo Linciano <lincianolorenzo3@outlook.it>
2023-08-03 17:16:53 +0200

    user story 7:back-end finito

--- app/Http/Livewire/ArticleForm.php
+++ app/Http/Livewire/ArticleForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\GoogleVisionSafeSearch;
 use App\Models\Article;
 use Livewire\Component;
 use App\Models\Category;
@@ -90,6 +91,7 @@ class ArticleForm extends Component
                         $newImage = $this->article->images()->create(['path'=> $image->store($newFileName, 'public')]);
 
                         dispatch(new ResizeImage($newImage->path, 400, 300));
+                        dispatch(new GoogleVisionSafeSearch($newImage->id));
                     }
 
                     File::deleteDirectory(storage_path('/app/livewire-tmp'));
