31ace5b829dd67ee9f38975345df9b940d6aebb6
Ab0027 <alfonsoborromeo@hotmail.it>
2023-08-04 19:07:04 +0200

    Modifiche zona revisore + zona annunci

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,35 +3,48 @@
 
     <div class="container-fluid py-5">
         <div class="row justify-content-center">
-            <div class="col-12 text-center">
+            <div class="col-12 col-sm-3 col-xl-3"></div>
+            <div class="col-12 col-sm-9 col-xl-9 text-center">
                 <h1 class="display-1 font-titolo">{{__('ui.allAnnouncements')}}</h1>
             </div>
         </div>
     </div>
 
         <div class="container w-100">
-            {{-- <div class="row justify-content-center h-100">
-           <div class="col-12 p-0">
-            <form class="d-flex" method="GET" action="{{route('article.search')}}">
-                <input type="text" placeholder="{{__('ui.searchHere')}}" name="searched">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-        </div> --}}
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-sm-3 col-xl-3 p-2">
+                    <form class="d-flex" method="GET" action="{{route('article.search')}}">
+                        <input class="form-control" type="text" placeholder="{{__('ui.searchHere')}}" name="searched">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <p class="text-center my-3">Filtra per categoria</p>
+                    @foreach ($categories as $category)
+                        <div class="form-check mt-3">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                {{$category->name}}
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
+            <div class="col-12 col-sm-9 col-xl-9">
+                <div class="row">
         @forelse ($articles as $article)
-     <div class="col-12 text-center my-4 d-flex justify-content-center">
-                <x-card 
-                :article="$article"
-
-                />
-            </div>
+                <div class="col-12 col-sm-6 col-xl-4 text-center my-4 d-flex justify-content-center">
+                    <x-card 
+                    :article="$article"
+                    />
+                </div>
             @empty
                 <div class="col-12">
                     <div class="alert alert-warning py-3 shadow">
                         <p class="lead">{{__('ui.empty')}}</p>
                     </div>
                 </div>
-            @endforelse
-            
+        @endforelse
+                </div>
+            </div>
+
             {{$articles->links()}}
             
         </div>
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -3,12 +3,12 @@
         <div class="container-fluid">
           <div class="row justify-content-center">
               <div class="col-12 text-center">
-                  <h1 class="display-1 font-titolo">{{$article_to_check ? __('ui.articleRevision') : __('ui.noAnnouncementRevisor')}}</h1>
+                  <h1 class="display-1 font-titolo py-4">{{$article_to_check ? __('ui.articleRevision') : __('ui.noAnnouncementRevisor')}}</h1>
               </div>
           </div>
       </div>
       @if ($article_to_check)
-        <div class = "card-wrapper-detail">
+        <div class = "card-wrapper-detail mt-2">
           <div class = "card">
             <!-- card left -->
             <div class = "product-imgs">
@@ -41,7 +41,7 @@
               
             @endif
             <!-- card right -->
-            <div class = "product-content">
+            <div class = "product-content text-center">
               <h2 class = "product-title">{{__('ui.title')}}: {{$article_to_check->title}}</h2>
               
     
@@ -67,16 +67,17 @@
                 <div class="row">
                   
                   <div class="col-12">
-                    <div class="product-detail">
-                      <h5 class="tc-accent">Revisione Immagini</h5>
-                      <p>Adulti: <span class="{{$image->adult}}"></span></p>
-                      <p>Satira: <span class="{{$image->spoof}}"></span></p>
-                      <p>Medicina: <span class="{{$image->medical}}"></span></p>
-                      <p>Violenza: <span class="{{$image->violence}}"></span></p>
-                      <p>Contenuto Ammiccante: <span class="{{$image->racy}}"></span></p>
+                    <div class="product-detail text-center">
+                      <h4 class="tc-accent pb-3">Revisione Immagini</h4>
+                      <span>Adulti: <span class="{{$image->adult}}"></span></span>
+                      <span>Satira: <span class="{{$image->spoof}}"></span></span>
+                      <span>Medicina: <span class="{{$image->medical}}"></span></span>
+                      <span>Violenza: <span class="{{$image->violence}}"></span></span>
+                      <br>
+                      <span>Contenuto Ammiccante: <span class="{{$image->racy}}"></span></span>
                     </div>
                   </div>
-                  <div class="col-12 text-center">
+                  <div class="col-12 text-center pt-5">
                     @if ($image->labels)
                       @foreach ($image->labels as $label)
                           <p class="d-inline">#{{$label}}</p>
